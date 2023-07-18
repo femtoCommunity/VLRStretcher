5a5404203579e3cba1724a4e79345c975ec90b54
Milkeyyy <59532514+Milkeyyy@users.noreply.github.com>
2023-07-18 22:42:39 +0900

    ふぁーすとこみっと

---
+++ Main.cs
@@ -0,0 +1,56 @@
+﻿using Produire;
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace VLRStretcher.Helper
+{
+	public class VLRStretcherHelper : IProduireStaticClass // これを実装しないとプロデルから利用できない
+	{
+		// 手順
+		[自分("を")]
+		public bool 実行する([省略][で] string プロセス名 = "VALORANT-Win64-Shipping")
+		{
+			return WindowHelper.Execute(プロセス名);
+		}
+	}
+
+	class WindowHelper
+	{
+		const int GWL_STYLE = -16;
+		const int WS_BORDER = 0x00800000;
+		const int SW_MAXIMIZE = 3;
+
+		[DllImport("user32.dll")]
+		static extern IntPtr GetWindowLongPtr(IntPtr hWnd, int nIndex);
+
+		[DllImport("user32.dll")]
+		static extern IntPtr SetWindowLongPtr(IntPtr hWnd, int nIndex, IntPtr dwNewLong);
+
+		[DllImport("user32.dll")]
+		static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+		public static bool Execute(string processName)
+		{
+			Process[] processes = Process.GetProcessesByName(processName);
+			if (processes.Length > 0)
+			{
+				IntPtr mainWindowHandle = processes[0].MainWindowHandle;
+				IntPtr windowStyle = GetWindowLongPtr(mainWindowHandle, GWL_STYLE);
+
+				// ウィンドウスタイルを変更
+				windowStyle = new IntPtr((long)windowStyle & ~WS_BORDER);
+				SetWindowLongPtr(mainWindowHandle, GWL_STYLE, windowStyle);
+
+				// ウィンドウを最大化
+				ShowWindow(mainWindowHandle, SW_MAXIMIZE);
+
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,41 @@
+﻿using Produire;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
+// 制御されます。アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更してください。
+[assembly: AssemblyTitle("VLRStretcher Helper Plugin")]
+[assembly: AssemblyDescription("VLRStretcher Helper Plugin")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Milkeyyy")]
+[assembly: AssemblyProduct("VLRStretcher")]
+[assembly: AssemblyCopyright("Copyright (C) 2023 Milkeyyy")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: PluginName("VLRStretcher Helper Plugin")]
+[assembly: PluginDescription("VLRStretcher Helper Plugin")]
+[assembly: PluginVersion("1.0")]
+[assembly: PluginAcceptVersion("1.0")]
+
+// ComVisible を false に設定すると、このアセンブリ内の型は COM コンポーネントから
+// 参照できなくなります。COM からこのアセンブリ内の型にアクセスする必要がある場合は、
+// その型の ComVisible 属性を true に設定してください。
+[assembly: ComVisible(false)]
+
+// このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
+//[assembly: Guid("8a16435c-ecab-4245-aea1-f33defebb52d")]
+
+// アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
+//
+//      メジャー バージョン
+//      マイナー バージョン
+//      ビルド番号
+//      リビジョン
+//
+// すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
+// 既定値にすることができます:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
